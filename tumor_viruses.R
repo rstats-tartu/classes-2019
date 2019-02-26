ee04f6709e1e37938fd681116122117b1b991ace
Taavi Päll <tapa741@gmail.com>
2019-02-26 18:07:56 +0200

    added tumor viruses

---
+++ tumor_viruses.R
@@ -0,0 +1,28 @@
+
+library(readr)
+library(stringr)
+
+tumor_viruses <- read_tsv("Tuumorviirus	Viirusperekond	Genoomi suurus (kb)\n
+B-hepatiidi viirus (HBV)	hepadna	3\n
+Merkeli rakkude polüoomi viirus (MCPyV) polyomaviridae	5\n
+inimese papilloomiviirus 16 (HPV)	papova	8\n
+inimese adenoviirus 5	adenoviirus	35\n
+inimese herpesviirus 8 (HSV-8; KSHV)	herpesviirused	165\n
+Shope fibroomiviirus	poxviirused	160\n
+Rousi sarkoomiviirus (RSV)	retroviirused	9\n
+inimese T-raku leukeemia viirus (HTLV-I)	retroviirus	9")
+
+
+viruses <- mutate_if(viruses, is.character, str_to_lower) 
+hbv <- viruses %>% 
+  filter(str_detect(organism_name, "hepatitis b"), str_detect(host, "human"))
+
+merkel <- viruses %>% 
+  filter(str_detect(organism_name, "merkel"), str_detect(host, "human"))
+
+hpv16 <- viruses %>% 
+  filter(str_detect(organism_name, "papilloma.+16"), str_detect(host, "human"))
+
+hadv <- viruses %>% 
+  filter(str_detect(organism_name, "adenov.+ 5$"), str_detect(host, "human")) %>% 
+  select(organism_name)
