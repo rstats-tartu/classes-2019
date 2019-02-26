c5f1dc147e3e598721ef9936d9b273220caea4cb
Taavi Päll <taavi74@ut.ee>
2019-02-26 19:21:01 +0200

    minor updates

--- tumor_viruses.R
+++ tumor_viruses.R
@@ -2,15 +2,16 @@
 library(readr)
 library(stringr)
 
-tumor_viruses <- read_tsv("Tuumorviirus	Viirusperekond	Genoomi suurus (kb)\n
-B-hepatiidi viirus (HBV)	hepadna	3\n
-Merkeli rakkude polüoomi viirus (MCPyV) polyomaviridae	5\n
-inimese papilloomiviirus 16 (HPV)	papova	8\n
-inimese adenoviirus 5	adenoviirus	35\n
-inimese herpesviirus 8 (HSV-8; KSHV)	herpesviirused	165\n
-Shope fibroomiviirus	poxviirused	160\n
-Rousi sarkoomiviirus (RSV)	retroviirused	9\n
-inimese T-raku leukeemia viirus (HTLV-I)	retroviirus	9")
+tumor_viruses <- read_csv("Tuumorviirus, Viirusperekond,  Genoomi suurus (kb)\n
+B-hepatiidi viirus (HBV),hepadna,3\n
+Merkeli rakkude polüoomi viirus (MCPyV), polyomaviridae, 5\n
+inimese papilloomiviirus 16 (HPV),papova,8\n
+inimese adenoviirus 5,adenoviirus,35\n
+inimese herpesviirus 8 (HSV-8; KSHV),herpesviirused,165\n
+Shope fibroomiviirus,poxviirused,160\n
+Rousi sarkoomiviirus (RSV),retroviirused,9\n
+inimese T-raku leukeemia viirus (HTLV-I),retroviirus,9")
+tumor_viruses
 
 
 viruses <- mutate_if(viruses, is.character, str_to_lower) 
